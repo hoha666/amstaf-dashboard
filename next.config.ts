b3878c383724961c7d2fbe1ab553f79189b79ab4
hoha666 <habil.harati@gmail.com>
2025-09-11 08:16:28 +0200

    allow render of images via minio

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,  // this forces Next.js to create folders per page
+    reactStrictMode: true,
+    output: "export",
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "minio.qa.amstaf.ir",
+                pathname: "/product-media/**",
+            },
+            // optional: local dev against MinIO
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "9000",
+                pathname: "/product-media/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
